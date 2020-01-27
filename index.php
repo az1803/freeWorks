a68edd93ffcb5c6c5d5bdfc3b98a9ba71f8c3335
az1803 <60303424+az1803@users.noreply.github.com>
2020-01-27 10:54:17 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatibe" content="ie=edge">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>Brooke's betting</title>
+</head>
+<body>
+	<header>
+		<div>
+			<h1>Brooke's Bet</h1>
+		</div>
+	</header>
+
+	<div class="container">
+		<div class="adbox"> walking pictures</div>
+	</div>
+	<div class="container">
+		<div class="small-box">Dayly</div>
+		<div class="small-box">Weekly</div>
+		<div class="small-box">Monthly</div>
+		<div class="small-box">Big Gain</div>
+	</div>
+	<div class="container">
+		<div class="tickets">pictures of old winning ticketes</div>
+		<div class="tickets">pictures of old winning ticketes</div>
+		<div class="tickets">pictures of old winning ticketes</div>
+	</div>
+	<footer>
+		<p>Powered by Aaz Enterpris</p>
+	</footer>
+</body>
+</html>
